de752d989e017e1e1b089bbb60beb245d6728ac1
abdelmanaf <ourotoumanaf6@gmail.com>
2022-11-27 20:27:20 -0500

    add questions array for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,28 @@ const questionContainerElement = document.getElementById('question-container');
 
 startButton.addEventListener('click', startGame);
 
+// questions lists
+const questions = [
+  {
+    question: 'When was JavaScript created?',
+    answers: [
+      {text: '1995', correct: true},
+      {text: '1994', correct: false},
+      {text: '1996', correct: false},
+      {text: '1993', correct: false}
+    ]
+  }
+]
+
+// display questions
+let shuffledQuestions
+let currentQuestionIndex
+
+
 function startGame(){
-  console.log('Game started');
   startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
