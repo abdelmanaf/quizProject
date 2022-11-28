95a0c85b2e752f99cf7e44ad2b695f9281de20d7
abdelmanaf <ourotoumanaf6@gmail.com>
2022-11-27 21:00:24 -0500

    complete set status class function

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,3 +67,15 @@ function selectAnswer(e){
   })
 }
 
+function setStatusClass(element, correct){
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(){
+
+}
